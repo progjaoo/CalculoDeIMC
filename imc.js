510cef0fde24cd5e1ad7e8f99152b09ce7879c9a
João Marcos dos Santos Valente <100044031+progjaoo@users.noreply.github.com>
2022-07-19 17:36:29 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,33 @@
+function calcularImc(){
+    let altura = document.getElementById('altura').value
+    let peso = document.getElementById('peso').value
+    let img = document.getElementById('img')
+
+    let imc = peso / (altura * altura).toFixed(2)
+
+    if( imc >= 0 && imc < 18.5){
+        img.src = 'img/magro.png'
+        document.body.style.background = '#5bd9ab'
+    }
+    else if( imc >= 18.5 && imc < 24.9){
+        img.src = 'img/peso-ideal.png'
+        Document.body.style.background = '1ba673'
+    }
+    else if( imc >= 25 && imc < 30){
+        img.src = 'img/acima do peso.png'
+        Document.body.style.background = '#f28f79'
+    }
+    else if( imc >= 30.1 && imc < 35){
+        img.src = 'img/gordo.png'
+        Document.body.style.background = '#f2637e'
+    }
+    else if( imc >= 35.1 && imc < 99){
+        img.src = 'img/obeso.png'
+        Document.body.style.background = '#d94e4e'
+    }
+    else{imc
+    }
+}
+
+let button = document.getElementById('button')
+button.addEventListener('click', calcularImc)
